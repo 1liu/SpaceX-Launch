f27244ea5447117d9ae3a310eb5e3c30449acb9d
Yizhou Liu <lewis.liu.china@gmail.com>
2020-03-24 23:58:40 -0700

    able to display launch site weather

--- components/app.js
+++ components/app.js
@@ -1,5 +1,5 @@
 class App {
-  constructor(upcomingTable, pageHeader, missionInfo, googleMap){
+  constructor(upcomingTable, pageHeader, missionInfo, googleMap, weather){
     this.launchData = null;
     this.pastData = null;
     this.site = null;
@@ -7,6 +7,7 @@ class App {
     this.pageHeader = pageHeader;
     this.missionInfo = missionInfo;
     this.googleMap = googleMap;
+    this.weather = weather;
 
     this.getData = this.getData.bind(this);
     this.handleGetDataError = this.handleGetDataError.bind(this);
@@ -15,6 +16,9 @@ class App {
     this.getPads = this.getPads.bind(this);
     this.handleGetPadsError = this.handleGetPadsError.bind(this);
     this.handleGetPadsSuccess = this.handleGetPadsSuccess.bind(this);
+    this.getWeather = this.getWeather.bind(this);
+    this.handleGetWeatherError = this.handleGetWeatherError.bind(this);
+    this.handleGetWeatherSuccess = this.handleGetWeatherSuccess.bind(this);
   }
 
   getData(){
@@ -36,6 +40,9 @@ class App {
     this.upcomingTable.updateTable(this.launchData);
     this.pageHeader.updateNextLaunch(this.launchData[0]);
     this.missionInfo.updateInfo(this.launchData[0]);
+    this.getPads(data[0].launch_site.site_id);
+
+    // this.weather.updateWeather(this.launchData[0]);
     // this.googleMap.updateMap(this.launchData[1]);
   }
 
@@ -84,14 +91,47 @@ class App {
     var lon = site.location.longitude;
     console.log("lat", lat);
     console.log("lon", lon);
+    this.getWeather(lat, lon);
+    // this.weather.updateWeather(lat, lon);
     // this.googleMap.updateMap(lat, lon);
 
   }
+
+  getWeather(lat, lon) {
+    // var apiKey = "0d5dc97fa06aa4c3b2346c3b831447c6";
+    // var url = "api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon +"&appid=0d5dc97fa06aa4c3b2346c3b831447c6";
+    // $.ajax({
+    //   "url": url,
+    //   "method": "GET",
+    //   dataType: "json",
+    $.ajax({
+      type: "POST",
+      url: "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=de6d52c2ebb7b1398526329875a49c57&units=metric",
+      dataType: "json",
+      success: this.handleGetWeatherSuccess,
+      error: this.handleGetWeatherError
+    })
+  }
+  handleGetWeatherError(error) {
+    console.error(error);
+  }
+  handleGetWeatherSuccess(data) {
+    // console.log(data);
+    this.weather.updateWeather(data);
+    // data.shift();
+    // this.launchData = data;
+    // this.upcomingTable.updateTable(this.launchData);
+    // this.pageHeader.updateNextLaunch(this.launchData[0]);
+    // this.missionInfo.updateInfo(this.launchData[0]);
+    // this.googleMap.updateMap(this.launchData[1]);
+  }
+
   launchClicked(data) {
     console.log("Clicked");
     console.log(data);
     this.missionInfo.updateInfo(data);
     this.getPads(data.launch_site.site_id);
+
     // this.googleMap.updateMap(data);
   }
 
--- main.js
+++ main.js
@@ -2,5 +2,6 @@ var upcomingTable = new UpcomingTable(document.querySelector("#upcoming-table"))
 var pageHeader = new PageHeader(document.querySelector("header"));
 var missionInfo = new MissionInfo(document.querySelector(".mission-info"));
 var googleMap = new GoogleMap(document.querySelector(".map-canvas"));
-var app = new App(upcomingTable, pageHeader, missionInfo, googleMap);
+var weather = new Weather(document.querySelector("#weather"));
+var app = new App(upcomingTable, pageHeader, missionInfo, googleMap, weather);
 app.start();
