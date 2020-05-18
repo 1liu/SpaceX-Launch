0a4c1d938cb484ec06812272cb93e5fa766badfc
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-05-18 16:27:29 -0700

    Converted to ES6 variable declaration

--- components/app.js
+++ components/app.js
@@ -75,8 +75,8 @@ class App {
     console.error(error);
   }
   handleGetPadsSuccess(site){
-    var lat = site.location.latitude;
-    var lon = site.location.longitude;
+    const lat = site.location.latitude;
+    const lon = site.location.longitude;
     this.getWeather(lat, lon);
     this.googleMap.updateMap(lat, lon);
 
@@ -107,9 +107,9 @@ class App {
     this.upcomingTable.onLaunchClick(this.launchClicked);
     this.getData();
     this.googleMap.initMap();
-    var pastLaunchMode = document.querySelector("#past-launch-mode");
+    const pastLaunchMode = document.querySelector("#past-launch-mode");
     pastLaunchMode.addEventListener("click", this.getPastData);
-    var upcomingLaunchMode = document.querySelector("#upcoming-launch-mode");
+    const upcomingLaunchMode = document.querySelector("#upcoming-launch-mode");
     upcomingLaunchMode.addEventListener("click", this.getData);
   }
 }
