a3bd913616663ffee69c9bd9f46ac7185a715101
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-05-18 15:56:40 -0700

    Calculated countdown using getTime

--- components/page-header.js
+++ components/page-header.js
@@ -4,22 +4,14 @@ class PageHeader{
   }
 
   updateNextLaunch(nextLaunch) {
-    console.log(nextLaunch);
     const launchTime = this.headerElement.querySelector("#next-launch-time");
     const nextLaunchDate = nextLaunch.launch_date_local.slice(0,10);
     launchTime.textContent = nextLaunchDate;
     const localLaunchTime = nextLaunch.launch_date_local;
     const countDownEl = this.headerElement.querySelector("#count-down");
 
-    const date = new Date();
-    const y = date.getFullYear();
-    const m = date.getMonth() + 1;
-    const d = date.getDate();
+    countDownEl.textContent = parseInt((new Date(localLaunchTime).getTime() - new Date().getTime())/(24*3600*1000));
 
-    const ly = Number(localLaunchTime.slice(0, 4));
-    const lm = Number(localLaunchTime.slice(5, 7));
-    const ld = Number(localLaunchTime.slice(8, 10));
-    countDownEl.textContent = (ly-y)*365+(lm-m)*30+(ld-d);
   }
 
 }
